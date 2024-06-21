85a65d0da16aa9fd9443d4dda757f25912de3ec1
rozqlinq <alkeyeva.rozalina@gmail.com>
2024-06-21 15:49:51 +0500

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import tkinter
-import tk
+import turtle
 from sklearn import metrics
 matplotlib.use('tkagg')
 
